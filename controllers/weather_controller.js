751570a88e4c3458946120d654af9f075899290b
Azmeer Siddiq <reemza77@gmail.com>
2017-10-20 21:43:20 +0800

    stable

--- controllers/weather_controller.js
+++ controllers/weather_controller.js
@@ -1,4 +1,4 @@
-const Weather = require('../models/Weather')
+
 const request= require('request')
 
 
--- indexWeather.js
+++ indexWeather.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/weather'
-const Weather = require('./models/Weather')
+//test data input to use with weather models
 
-// console.log(Movie);
-
-mongoose.connect(url, {
-  useMongoClient:true
-})
-
-mongoose.Promise = global.Promise
-
-
-
-var newWeather = new Weather({
-country:'Singapore',
-description: 'Super hot',
-precipitation: 23,
-max_temp: 32,
-min_temp: 20,
-})
-
-//created newMovie in BSON format. binary, due to the id generated(diff from JSON)
-// console.log(newMovie);
-
-newWeather.save(function (err,data){
-  if(err) throw err;
-
-  console.log('new weather created');
-  console.log(data);
-})
+// const mongoose = require('mongoose')
+// const url = 'mongodb://localhost:27017/weather'
+// const Weather = require('./models/Weather')
+//
+// mongoose.connect(url, {
+//   useMongoClient:true
+// })
+//
+// mongoose.Promise = global.Promise
+//
+//
+//
+// var newWeather = new Weather({
+// country:'Singapore',
+// description: 'Super hot',
+// precipitation: 23,
+// max_temp: 32,
+// min_temp: 20,
+// })
+//
+//
+//
+// newWeather.save(function (err,data){
+//   if(err) throw err;
+//
+//   console.log('new weather created');
+//   console.log(data);
+// })
